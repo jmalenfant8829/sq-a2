5969b702e777f5e0c9d2250fdf7a60c347c1050e
jmalenfant8829 <jmalenfant8829@conestogac.on.ca>
2018-11-21 12:45:59 -0500

    Added several UI elements to form

--- TriangleCalculator/TriangleForm.Designer.cs
+++ TriangleCalculator/TriangleForm.Designer.cs
@@ -28,33 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnSubmit = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.calcRightTriBtn = new System.Windows.Forms.Button();
+            this.side2Box = new System.Windows.Forms.TextBox();
+            this.side1Box = new System.Windows.Forms.TextBox();
+            this.hypBox = new System.Windows.Forms.TextBox();
+            this.areaBox = new System.Windows.Forms.TextBox();
+            this.angle1Box = new System.Windows.Forms.TextBox();
+            this.angle2Box = new System.Windows.Forms.TextBox();
+            this.angleResultbox = new System.Windows.Forms.TextBox();
+            this.calcAngleBtn = new System.Windows.Forms.Button();
+            this.rightTriCalcLbl = new System.Windows.Forms.Label();
+            this.calcAngleLbl = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // btnSubmit
+            // calcRightTriBtn
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(622, 330);
-            this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
-            this.btnSubmit.TabIndex = 0;
-            this.btnSubmit.Text = "Submit";
-            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.calcRightTriBtn.Location = new System.Drawing.Point(381, 119);
+            this.calcRightTriBtn.Name = "calcRightTriBtn";
+            this.calcRightTriBtn.Size = new System.Drawing.Size(75, 23);
+            this.calcRightTriBtn.TabIndex = 0;
+            this.calcRightTriBtn.Text = "Submit";
+            this.calcRightTriBtn.UseVisualStyleBackColor = true;
             // 
-            // textBox1
+            // side2Box
             // 
-            this.textBox1.Location = new System.Drawing.Point(270, 278);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 1;
+            this.side2Box.Location = new System.Drawing.Point(225, 119);
+            this.side2Box.Name = "side2Box";
+            this.side2Box.Size = new System.Drawing.Size(100, 20);
+            this.side2Box.TabIndex = 1;
+            // 
+            // side1Box
+            // 
+            this.side1Box.Location = new System.Drawing.Point(89, 119);
+            this.side1Box.Name = "side1Box";
+            this.side1Box.Size = new System.Drawing.Size(100, 20);
+            this.side1Box.TabIndex = 3;
+            // 
+            // hypBox
+            // 
+            this.hypBox.Location = new System.Drawing.Point(502, 119);
+            this.hypBox.Name = "hypBox";
+            this.hypBox.ReadOnly = true;
+            this.hypBox.Size = new System.Drawing.Size(100, 20);
+            this.hypBox.TabIndex = 4;
+            // 
+            // areaBox
+            // 
+            this.areaBox.Location = new System.Drawing.Point(502, 167);
+            this.areaBox.Name = "areaBox";
+            this.areaBox.ReadOnly = true;
+            this.areaBox.Size = new System.Drawing.Size(100, 20);
+            this.areaBox.TabIndex = 5;
+            // 
+            // angle1Box
+            // 
+            this.angle1Box.Location = new System.Drawing.Point(89, 323);
+            this.angle1Box.Name = "angle1Box";
+            this.angle1Box.Size = new System.Drawing.Size(100, 20);
+            this.angle1Box.TabIndex = 6;
+            // 
+            // angle2Box
+            // 
+            this.angle2Box.Location = new System.Drawing.Point(225, 323);
+            this.angle2Box.Name = "angle2Box";
+            this.angle2Box.Size = new System.Drawing.Size(100, 20);
+            this.angle2Box.TabIndex = 7;
+            // 
+            // angleResultbox
+            // 
+            this.angleResultbox.Location = new System.Drawing.Point(501, 322);
+            this.angleResultbox.Name = "angleResultbox";
+            this.angleResultbox.ReadOnly = true;
+            this.angleResultbox.Size = new System.Drawing.Size(100, 20);
+            this.angleResultbox.TabIndex = 8;
+            // 
+            // calcAngleBtn
+            // 
+            this.calcAngleBtn.Location = new System.Drawing.Point(381, 323);
+            this.calcAngleBtn.Name = "calcAngleBtn";
+            this.calcAngleBtn.Size = new System.Drawing.Size(75, 23);
+            this.calcAngleBtn.TabIndex = 9;
+            this.calcAngleBtn.Text = "Submit";
+            this.calcAngleBtn.UseVisualStyleBackColor = true;
+            // 
+            // rightTriCalcLbl
+            // 
+            this.rightTriCalcLbl.AutoSize = true;
+            this.rightTriCalcLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rightTriCalcLbl.Location = new System.Drawing.Point(45, 39);
+            this.rightTriCalcLbl.Name = "rightTriCalcLbl";
+            this.rightTriCalcLbl.Size = new System.Drawing.Size(732, 31);
+            this.rightTriCalcLbl.TabIndex = 10;
+            this.rightTriCalcLbl.Text = "Enter numbers to calculate area/hypotenuse of right triangle";
+            // 
+            // calcAngleLbl
+            // 
+            this.calcAngleLbl.AutoSize = true;
+            this.calcAngleLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcAngleLbl.Location = new System.Drawing.Point(133, 250);
+            this.calcAngleLbl.Name = "calcAngleLbl";
+            this.calcAngleLbl.Size = new System.Drawing.Size(469, 31);
+            this.calcAngleLbl.TabIndex = 11;
+            this.calcAngleLbl.Text = "Enter numbers to calculate third angle";
+            this.calcAngleLbl.Click += new System.EventHandler(this.label1_Click);
             // 
             // TriangleForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.calcAngleLbl);
+            this.Controls.Add(this.rightTriCalcLbl);
+            this.Controls.Add(this.calcAngleBtn);
+            this.Controls.Add(this.angleResultbox);
+            this.Controls.Add(this.angle2Box);
+            this.Controls.Add(this.angle1Box);
+            this.Controls.Add(this.areaBox);
+            this.Controls.Add(this.hypBox);
+            this.Controls.Add(this.side1Box);
+            this.Controls.Add(this.side2Box);
+            this.Controls.Add(this.calcRightTriBtn);
             this.Name = "TriangleForm";
             this.Text = "Triangle Calculator";
             this.ResumeLayout(false);
@@ -64,8 +157,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnSubmit;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button calcRightTriBtn;
+        private System.Windows.Forms.TextBox side2Box;
+        private System.Windows.Forms.TextBox side1Box;
+        private System.Windows.Forms.TextBox hypBox;
+        private System.Windows.Forms.TextBox areaBox;
+        private System.Windows.Forms.TextBox angle1Box;
+        private System.Windows.Forms.TextBox angle2Box;
+        private System.Windows.Forms.TextBox angleResultbox;
+        private System.Windows.Forms.Button calcAngleBtn;
+        private System.Windows.Forms.Label rightTriCalcLbl;
+        private System.Windows.Forms.Label calcAngleLbl;
     }
 }
 
